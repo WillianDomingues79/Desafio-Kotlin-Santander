cdb2be4fd12de0100e1b41892801c0ca87be50e4
WillianDomingues79 <willian.domingues3@gmail.com>
2023-09-14 18:41:02 -0300

    Adicionado Resolução do Desafio3, Desafio4 e Desafio5

--- app/src/main/java/com/example/desafiokotlin/Main.kt
+++ app/src/main/java/com/example/desafiokotlin/Main.kt
@@ -1,8 +1,15 @@
 package com.example.desafiokotlin
 
+import java.text.DecimalFormat
+import java.util.Scanner
+import kotlin.math.pow
+
 fun main(){
-    Desafio1()
-    Desafio2()
+    //Desafio1()
+    //Desafio2()
+    //Desafio3()
+    //Desafio4()
+    Desafio5()
 }
 
 fun Desafio1() {
@@ -35,5 +42,56 @@ fun Desafio2(){
     //TODO: Imprimir a lista de ativos ordenada, conforme a tabela de exemplos.
     for (ativo in ordenados) {
         println(ativo)
+    }
+}
+
+fun Desafio3() {
+    // Entrada de dados
+    val saldoTotal = readLine() !!.toInt()
+    val valorSaque = readLine() !!.toInt()
+
+    //TODO: Criar as condições necessárias para impressão da saída, vide tabela de exemplos.
+    val saldoAtual = saldoTotal - valorSaque
+    if(saldoTotal >= valorSaque){
+        println("Saque realizado com sucesso. Novo saldo: ${saldoAtual}")
+    }else{
+        println("Saldo insuficiente. Saque nao realizado!")
+    }
+}
+
+fun Desafio4() {
+    val scanner = Scanner(System.`in`)
+    val df = DecimalFormat("#.00")
+
+    val valorInicial = scanner.nextDouble()
+
+    val taxaJuros = scanner.nextDouble()
+
+    val periodo = scanner.nextInt()
+
+    //TODO: Iterar, baseado no período em anos, para calculo do valorFinal com os juros.
+    var valorFinal = valorInicial * (1 + taxaJuros).pow(periodo)
+
+    println("Valor final do investimento: R$ ${df.format(valorFinal)}")
+
+    scanner.close()
+}
+
+fun Desafio5() {
+    val scanner = Scanner(System.
+    `in`)
+    val valor = scanner.nextDouble()
+    val df = DecimalFormat("#.00")
+
+    if (valor > 0) {
+        //TODO: Imprimir a mensagem de sucesso, formatando o saldo atual (vide Exemplos).
+        println("Deposito realizado com sucesso!\n" +
+                "Saldo atual: R$ ${df.format(valor)}")
+    } else if (valor == 0.0) {
+        //TODO: Imprimir a mensagem de valor inválido.
+        println("Encerrando o programa...")
+    } else {
+        //TODO: Imprimir a mensagem de encerrar o programa.
+        println("Valor invalido! Digite um valor maior que zero.")
     }
 }
